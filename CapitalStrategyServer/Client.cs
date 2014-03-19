5760f733dbfd263a7f0a61b2c8b942b36115f3cc
Cameron Will <cwill747@gmail.com>
2014-03-18 20:51:03 -0400

    Add base CapitalStrategyServer Class
    Add base server class, successfully listens for messages from clients
    and includes the base Message code

---
+++ CapitalStrategyServer/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalStrategyServer
+{
+    class Client
+    {
+        public string uniqueIdentifier { get; set; }
+        public bool inGame { get; set; }
+    }
+}
---
+++ CapitalStrategyServer/QueueManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalStrategyServer
+{
+    class QueueManager
+    {
+        public List<Client> clientsConnected { get; set; }
+
+        public QueueManager()
+        {
+            clientsConnected = new List<Client>();
+        }
+        public void clientConnected(string identifier)
+        {
+            Client c = new Client();
+            c.uniqueIdentifier = identifier;
+            clientsConnected.Add(c);
+        }
+    }
+}
