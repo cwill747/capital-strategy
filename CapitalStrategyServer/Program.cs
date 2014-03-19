4cdf238eaa315c4539e1a21d24415cc4dbdd4a50
Cameron Will <cwill747@gmail.com>
2014-03-18 20:54:40 -0400

    Forgot to commit some Server Stuff

--- CapitalStrategyServer/Program.cs
+++ CapitalStrategyServer/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
+using Lidgren.Network;
 
 namespace CapitalStrategyServer
 {
@@ -10,6 +9,104 @@ namespace CapitalStrategyServer
     {
         static void Main(string[] args)
         {
+            NetPeerConfiguration config = new NetPeerConfiguration("xnaapp");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            config.Port = 14242;
+            QueueManager qm = new QueueManager();
+            // create and start server
+            NetServer server = new NetServer(config);
+            server.Start();
+
+            // schedule initial sending of position updates
+            double nextSendUpdates = NetTime.Now;
+
+            // run until escape is pressed
+            while (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                NetIncomingMessage msg;
+                while ((msg = server.ReadMessage()) != null)
+                {
+                    switch (msg.MessageType)
+                    {
+                        case NetIncomingMessageType.DiscoveryRequest:
+                            //
+                            // Server received a discovery request from a client; send a discovery response (with no extra data attached)
+                            //
+                            //server.SendDiscoveryResponse(null, msg.SenderEndpoint);
+                            break;
+                        case NetIncomingMessageType.VerboseDebugMessage:
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                            //
+                            // Just print diagnostic messages to console
+                            //
+                            Console.WriteLine(msg.ReadString());
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)msg.ReadByte();
+                            if (status == NetConnectionStatus.Connected)
+                            {
+                                //
+                                // A new player just connected!
+                                //
+                                qm.clientConnected(NetUtility.ToHexString(msg.SenderConnection.RemoteUniqueIdentifier));
+                                Console.WriteLine(NetUtility.ToHexString(msg.SenderConnection.RemoteUniqueIdentifier) + " connected!");
+                            }
+
+                            break;
+                        case NetIncomingMessageType.Data:
+                            //
+                            // The client sent input to the server
+                            //
+                            Message m = new Message();
+                            msg.ReadAllFields(m);
+                            Console.WriteLine("Received Message: " + m.ToString());
+                            break;
+                    }
+
+                    //
+                    // send position updates 30 times per second
+                    //
+                    double now = NetTime.Now;
+                    if (now > nextSendUpdates)
+                    {
+                        // Yes, it's time to send position updates
+
+                        // for each player...
+                        foreach (NetConnection player in server.Connections)
+                        {
+                            // ... send information about every other player (actually including self)
+                            foreach (NetConnection otherPlayer in server.Connections)
+                            {
+                                // send position update about 'otherPlayer' to 'player'
+                                NetOutgoingMessage om = server.CreateMessage();
+
+                                // write who this position is for
+                                om.Write(otherPlayer.RemoteUniqueIdentifier);
+
+                                if (otherPlayer.Tag == null)
+                                    otherPlayer.Tag = new int[2];
+
+                                int[] pos = otherPlayer.Tag as int[];
+                                om.Write(pos[0]);
+                                om.Write(pos[1]);
+
+                                // send message
+                                server.SendMessage(om, player, NetDeliveryMethod.Unreliable);
+                            }
+                        }
+
+                        // schedule next update
+                        nextSendUpdates += (1.0 / 30.0);
+                    }
+                }
+
+                // sleep to allow other processes to run smoothly
+                Thread.Sleep(1);
+            }
+
+            server.Shutdown("app exiting");
         }
     }
 }
