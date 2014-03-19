714cdda733aca6ecfe5aa8231faaf31983216962
Cameron Will <cwill747@gmail.com>
2014-03-18 20:52:43 -0400

    First commit of the Sending Tester Class
    Basically allows for the sending of simple messages

---
+++ Lidgren Sending Tester/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+using System.Threading;
+using CapitalStrategyServer;
+
+namespace Lidgren_Sending_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("xnaapp");
+            NetClient client = new NetClient(config);
+            client.Start();
+            client.Connect("127.0.0.1", 14242);
+            string line;
+            do {
+                CapitalStrategyServer.Message m = new CapitalStrategyServer.Message();
+                Console.WriteLine("Message type: ");
+                line = Console.ReadLine();
+                m.msgType = line;
+                Console.WriteLine("Message information: ");
+                line = Console.ReadLine();
+                m.msg = line;
+                NetOutgoingMessage msg = client.CreateMessage();
+                msg.WriteAllFields(m);
+                client.SendMessage(msg, NetDeliveryMethod.ReliableUnordered);
+                client.FlushSendQueue();
+            } while (line != null);   
+         
+
+        }
+    }
+}
