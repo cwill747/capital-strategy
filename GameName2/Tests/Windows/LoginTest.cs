9af698db949521032da1e13555b2d9f126c9831c
Cameron Will <cwill747@gmail.com>
2014-03-30 15:23:46 -0400

    Add NUnit nuget package and some base test code
    Nuget NUnit package added to all projects, and the base test code for
    the login page.

---
+++ GameName2/Tests/Windows/LoginTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CapitalStrategy.Tests.Windows
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        [Test]
+        public void login()
+        {
+
+        }
+    }
+}
