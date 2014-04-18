87dc91dc514feb281eb01c26a2f3ec1d5cabcfd0
Cameron Will <cwill747@gmail.com>
2014-04-18 00:59:35 -0400

    Added username to "Waiting for player" in gamematch. Closes #21

--- GameName2/Windows/GameMatch.cs
+++ GameName2/Windows/GameMatch.cs
@@ -698,7 +698,7 @@ namespace CapitalStrategy.Windows
             }
             this.drawInfoFrame(this.selectedWarrior);
             this.spriteBatch.Begin();
-            string turnInfo = (this.isYourTurn) ? "It is your turn" : "Waiting for opponent";
+            string turnInfo = (this.isYourTurn) ? "It is your turn." : "Waiting for " + this.windowManager.otherPlayer.username + ".";
             //+ this.windowManager.otherPlayer.username
             this.spriteBatch.DrawString(Game1.smallFont, turnInfo, new Vector2(SELECTED_WARRIOR_INFO_X, SELECTED_WARRIOR_INFO_Y), Color.Brown);
 
